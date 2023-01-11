5feb7a00ac302000ea937f9b7a1eac19d40b9432
getill <98155180+getill@users.noreply.github.com>
2023-01-11 22:30:44 +0100

    You can now add your favorite films to your list !

--- src/pages/CoupDeC.js
+++ src/pages/CoupDeC.js
@@ -1,11 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
+import Card from "../components/Card";
 
 const CoupDeC = () => {
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    let filmId = window.localStorage.film
+      ? window.localStorage.film.split(",")
+      : [];
+    // Tant qu'il y a des films id, fetch moi leurs infos
+    for (let i = 0; i < filmId.length; i++) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${filmId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d`
+        )
+        // Destructuration + ajout de l'autre film, sinon la première donnée écrase l'autre
+        .then((res) => setListData((listData) => [...listData, res.data]));
+    }
+  }, []);
+
   return (
-    <div>
-      CDC
+    <div className="user-list-page">
       <Header />
+      <h2>
+        Coups de coeur <span>❤</span>
+      </h2>
+      <div className="result">
+        {listData.length > 0 ? (
+          listData.map((film) => <Card film={film} key={film.id} />)
+        ) : (
+          <h2> Aucun coup de coeur pour le moment</h2>
+        )}
+      </div>
     </div>
   );
 };
